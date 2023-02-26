f98c9bf9526e1cd03710a1c020eb48c31c169a5f
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-26 08:22:29 -0800

    working on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Homepage() {
     return (
-        <div>Homepage</div>
+        <div className="flex items-center justify-center 
+        flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+                Samer Mahir
+            </h1>
+            
+        </div>
     )
 }
 
