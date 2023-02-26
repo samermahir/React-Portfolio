3c82b3480f31e7c08d3e4c7cd0a3ba87b5066e29
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-26 08:42:35 -0800

    folder

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,7 +7,7 @@ function Homepage() {
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
                 Samer Mahir
             </h1>
-            <p>Manufacturing Operator & Software Developer</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Manufacturing Operator & Software Developer</p>
             <p className="text-sm max-w-xl mb-6 font-bold">My name is Samer Mahir, and I am currently enrolled in the UCI web developer bootcamp.
              I have 13 years experiance in the customer service sector and 
              5 years of manufacturing experience as a machine operator for International Paper.</p>
