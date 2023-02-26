1299affebd8b6627d676f9283a1ffdcbba315cff
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-26 08:50:49 -0800

    projects

---
+++ src/components/Projectdata/project.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'HappenIn',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript',],
+        link: 'https://zaksalazar.github.io/HappenIn/',
+    },
+    {
+        title: 'Gymder',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Handlebars',],
+        link: 'https://gymder-project.herokuapp.com/login',
+    },
+    {
+        title: 'HappenIn',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript',],
+        link: 'https://zaksalazar.github.io/HappenIn/',
+    },
+]
