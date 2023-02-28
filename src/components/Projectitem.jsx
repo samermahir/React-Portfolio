06c820f5ce2de1940e43540f232e501d75256c5f
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-27 16:17:13 -0800

    project

--- src/components/Projectitem.jsx
+++ src/components/Projectitem.jsx
@@ -6,7 +6,16 @@ function ProjectItem({ title, imgUrl, stack, link}) {
             <img src={imgUrl}
              alt="projects"
             className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
-        <div className="w-full"></div>
+        <div className="w-full p-4">
+            <h3>{title}</h3>
+            <p>
+                {stack.map(item => (
+                    <span>
+                        {item}
+                    </span>
+                ))}
+            </p>
+        </div>
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import project from "./Projectdata/project";
+import ProjectItem from "./Projectitem";
 
 function Projects() {
     return (
-        <div>Projects</div>
+        <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {project.map(portfolio => (
+                    <ProjectItem
+                        imgUrl={portfolio.imgUrl}
+                        title={portfolio.title}
+                        stack={portfolio.stack}
+                        link={portfolio.link}
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
 
