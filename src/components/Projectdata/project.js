8c4532048bd73593c56adbab3ca612962601b3a8
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-27 16:35:28 -0800

    adding project imgs

--- src/components/Projectdata/project.js
+++ src/components/Projectdata/project.js
@@ -7,19 +7,19 @@ export default [
     },
     {
         title: 'HappenIn',
-        imgUrl: '',
+        imgUrl: 'src/assets/Screen Shot 2023-02-27 at 4.29.57 PM.png',
         stack: ['HTML', 'CSS', 'JavaScript',],
         link: 'https://zaksalazar.github.io/HappenIn/',
     },
     {
         title: 'Gymder',
-        imgUrl: '',
+        imgUrl: 'src/assets/Screen Shot 2023-01-22 at 5.49.26 PM.png',
         stack: ['HTML', 'CSS', 'JavaScript', 'Handlebars',],
         link: 'https://gymder-project.herokuapp.com/login',
     },
     {
         title: 'Coding Quiz',
-        imgUrl: '',
+        imgUrl: 'src/assets/Screen Shot 2022-10-24 at 6.12.32 PM.png',
         stack: ['HTML', 'CSS', 'JavaScript',],
         link: 'https://samermahir.github.io/hw-4-quiz/',
     },
