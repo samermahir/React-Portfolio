f4247b758e8f756975b6330cf1adc7d773e65ec5
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-27 16:04:04 -0800

    items

--- src/components/Projectitem.jsx
+++ src/components/Projectitem.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function ProjectItem({ title, imgUrl, stack, link}) {
     return (
-        <div>Projects</div>
+        <div>
+            <img src={imgUrl}
+             alt="projects"
+            className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
+        <div className="w-full"></div>
+        </div>
     )
 }
 
