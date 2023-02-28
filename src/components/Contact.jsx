8bab69dec65675902d448ee43f67f6c73caa92c4
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-27 16:53:46 -0800

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,22 @@ import React from "react";
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <div className="flex flex-col mb-10 mx-auto">
+            <div className="flex justify-center items-center">
+                <form 
+                action="https://getform.io/slug"
+                method="POST"
+                className="flex flex-col w-full md:w-7/12"
+                >
+                    <input 
+                    type="text"
+                    name="name" 
+                    placeholder="Name
+                    className="p-2
+                    />
+                </form>
+            </div>
+        </div>
     )
 }
 
