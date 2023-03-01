f553003d04085ff50613f641299d1c88e973c0a2
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-28 16:10:32 -0800

    contacts and project.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,9 +12,33 @@ function Contact() {
                     <input 
                     type="text"
                     name="name" 
-                    placeholder="Name
-                    className="p-2
+                    placeholder="Name"
+                    className="p-2 bg-transparent border-2
+                    rounded-md focus:outline-none"
                     />
+                     <input 
+                    type="text"
+                    name="email" 
+                    placeholder="Email"
+                    className="my-2 p-2 bg-transparent border-2 
+                    rounded-md focus:outline-none"
+                    />
+                    <textarea 
+                        name="message"
+                        placeholder="Message" 
+                        rows="10"
+                        className="p-2 mb-4 bg-transparent border-2 
+                        rounded-md focus:outline-none"
+                        />
+                        <button 
+                        type="button"
+                        className="text-center inline-block px-8
+                        py-3 w-max text-base font-medium rounded-md
+                        text-white bg-gradient-to-r from-gray-500 to-blue-500
+                        drop-shadow-md">
+                            Message Me
+                        </button>
+                    
                 </form>
             </div>
         </div>
