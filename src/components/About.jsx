d7f3d51ab85adb40ee665f382442079a74595f18
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-03-07 17:10:48 -0800

    profile picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Title from "./Title";
+// import Title from "./Title";
+import IMG_4418 from "/assets/IMG_4418.jpg";
+
 
 function About() {
     return (
@@ -10,6 +12,9 @@ function About() {
             <p className="text-lg max-w-xl mb-6 font-bold text-center">My name is Samer Mahir, and I am currently enrolled in the UCI web developer bootcamp.
              I have 13 years experience in the customer service sector and 
              5 years of manufacturing experience as a machine operator for International Paper.</p>
+             
+             <img src={IMG_4418} alt="IMG_4418" />
+             
              {/* <a href="mailto:samermahir@yahoo.com"
              className="block mt-3 text-md md:text-lg font-bold
              font-regular text-gray-500 hover:text-blue-500">samermahir@yahoo.com</a> */}
